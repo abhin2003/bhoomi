6a5e02856888c33faaa887b1de82c09299c0a027
abhin <strivecad@gmail.com>
2024-05-09 13:33:47 +0530

    designed

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,42 +1,36 @@
 import React, { useState } from "react";
 import "./Card.css";
 import Button from "../Button/Button";
-function Card({ food , onAdd, onRemove }) {
+
+function Card({ food, onAdd, onRemove }) {
   const [count, setCount] = useState(0);
   const { title, Image, price, id } = food;
 
   const handleIncrement = () => {
     setCount(count + 1);
-    onAdd(food)
-    
+    onAdd(food);
   };
+
   const handleDecrement = () => {
     setCount(count - 1);
-    onRemove(food)
-    
+    onRemove(food);
   };
 
   return (
     <div className="card">
-      <span
-        className={`${count !== 0 ? "card__badge" : "card__badge--hidden"}`}
-      >
+      <span className={`card__badge ${count !== 0 ? "" : "card__badge--hidden"}`}>
         {count}
       </span>
       <div className="image__container">
         <img src={Image} alt={title} />
       </div>
-      <h4 className="card__title">
-        {title} . <span className="card__price">$ {price}</span>
-      </h4>
-
+      <div className="details">
+        <h4 className="card__title">{title}</h4>
+        <span className="card__price">${price}</span>
+      </div>
       <div className="btn-container">
-        <Button title={"+"} type={"add"} onClick={handleIncrement} />
-        {count !== 0 ? (
-          <Button title={"-"} type={"remove"} onClick={handleDecrement} />
-        ) : (
-          ""
-        )}
+        <Button title="+" type="add" onClick={handleIncrement} />
+        {count !== 0 && <Button title="-" type="remove" onClick={handleDecrement} />}
       </div>
     </div>
   );
