061a272ccb750fb973703dd99aa4a8f9daa92954
abhin <strivecad@gmail.com>
2024-05-09 12:15:17 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
+import React, { useState } from 'react'; // Import useState from React
 
 import './App.css';
-import Button from './Components/Button/Button';
 import Card from './Components/Card/Card';
 import Cart from "./Components/Cart/Cart";
 import { getData } from './db/db';
-
-const foods =getData()
-
-
+const foods = getData();
 
 function App() {
-  return(
-  <>
-    <h1 className='heading'>Order Foood</h1>
+  const [cartItems, setCartItems] = useState([]); // Now useState is defined
 
-<div className="cards__container">
+  const onAdd = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...food, quantity: 1 }]);
+    }
+  };
 
-{foods.map((food)=>{
-  return<Card food={food} key={food.id} />
-})}
-</div>
-</>
+  const onRemove = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== food.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        )
+      );
+    }
+  };
+
+  return (
+    <>
+      <h1 className="heading">Order Food</h1>
+      <Cart cartItems={cartItems} onAdd={onAdd} onRemove={onRemove}/>
+      <div className="cards__container">
+        {foods.map((food) => {
+          return <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />;
+        })}
+      </div>
+    </>
   );
 }
 
