276afdb14f2ae773c4c900c4438c37923fbb3191
abhin <strivecad@gmail.com>
2024-05-09 13:13:04 +0530

    bot connect2

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -5,8 +5,12 @@ const bot = new Telegraf(TOKEN)
 
 const web_link = ''
 
-bot.start((ctx) => ctx.reply('Welcome Sir', {reply_markup{keyboard:[[{text:'Web app',web_app:{url:web_link}]]}
-})
+bot.start((ctx) => 
+    ctx.reply('Welcome Sir', {
+        reply_markup: {
+            keyboard:[[{ text : 'Web app' , web_app: {url:web_link} }]],
+    },
+    })
 );
 
 bot.launch()
